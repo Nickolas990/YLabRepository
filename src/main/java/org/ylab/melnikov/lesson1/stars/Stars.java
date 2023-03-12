e4ad81169b6d24bebf7f60d214bd722e20a10604
Nickolas990 <hrom_90@hotmail.com>
2023-03-12 13:00:38 +0300

    lesson 2 started

---
+++ src/main/java/org/ylab/melnikov/lesson1/stars/Stars.java
@@ -0,0 +1,26 @@
+package org.ylab.melnikov.lesson1.stars;
+
+import java.util.Scanner;
+
+/**
+ * @author Nikolay Melnikov
+ */
+public class Stars {
+    public static void main(String[] args) {
+        Stars star = new Stars();
+        try (Scanner scanner = new Scanner(System.in)) {
+            int n = scanner.nextInt();
+            int m = scanner.nextInt();
+            String template = scanner.next();
+            star.print(n, m, template);
+        }
+    }
+    public void print(int n, int m, String template) {
+        for (int i = 0; i < n ; i++) {
+            for (int j = 0; j < m ; j++) {
+                System.out.print(template + " ");
+            }
+            System.out.println();
+        }
+    }
+}
